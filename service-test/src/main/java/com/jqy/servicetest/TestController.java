bfca877125a26e42d715aed5b96b6b3c8ac5db54
jqy <837292928@qq.com>
2019-11-22 12:51:20 +0800

    eureka-service

---
+++ service-test/src/main/java/com/jqy/servicetest/TestController.java
@@ -0,0 +1,12 @@
+package com.jqy.servicetest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String index(){
+        return "Hello!";
+    }
+}
