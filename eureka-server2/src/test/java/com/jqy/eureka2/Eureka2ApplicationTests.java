bc3bed6c67ae998a2ce9ef23ca588cadb2fc88f5
jqy <837292928@qq.com>
2019-11-22 11:17:18 +0800

    Initial commit

---
+++ eureka-server2/src/test/java/com/jqy/eureka2/Eureka2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jqy.eureka2;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eureka2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
